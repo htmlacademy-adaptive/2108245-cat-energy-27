fe9e35131e1afad60484204e11cd4c04763d6ef0
Alina Butlak <atm05fera@yandex.ru>
2023-02-11 00:29:07 +0300

    неполная адаптация главной страницы

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,7 @@ const server = (done) => {
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
   gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/js/*.js').on('change', browser.reload);
 }
 
 
